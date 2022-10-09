199913459e61cca2f516810f02aa03b1b6dd9d53
Falah Rafif <falah.rfif@gmail.com>
2022-10-09 20:45:21 +0700

    initial repo

---
+++ Controllers/CoffeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cofiel.Models;
+using Cofiel.Services;
+
+namespace Cofiel.Controllers
+{
+    public class CoffeeController : ICoffeeService
+    {
+        private List<Coffee> _Coffees = new List<Coffee>
+        {
+            new Coffee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kopi Hitam"
+            },
+            new Coffee
+            {
+                Id = Guid.NewGuid(),
+                Name = "Kopi Abu-Abu"
+            }
+        };
+
+        public List<Coffee> GetCoffees()
+        {
+            return _Coffees;
+        }
+    }
+}
---
+++ Services/ICoffeeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cofiel.Models;
+
+namespace Cofiel.Services
+{
+    interface ICoffeeService
+    {
+        List<Coffee> GetCoffees();
+    }
+}
